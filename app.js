3387ee55d77cf77915452fc64fc6cd1dfe14ddfd
Chris Sharkey <ev2900@gmail.com>
2017-01-03 12:50:09 -0500

    simple route

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Connect to Mongoose 
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connect
+
+app.get('/', function() {
+	res.send('Hello World!');
+});
+
+app.listen(3000);
+console.log('Running on port 3000');
