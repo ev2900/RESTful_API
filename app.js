27d55d5d18125f0663e4b5e08a4307b8c6202334
Chris Sharkey <ev2900@gmail.com>
2017-01-03 16:55:45 -0500

    Added put request

--- app.js
+++ app.js
@@ -67,6 +67,22 @@ var router = express.Router();
 				if(err)
 					res.send(err);
 				res.json(contact);
+			})
+		})
+
+		.put(function(req, res){
+			Contact.findById(req.params.contact_id, function(err, contact) {
+				if(err)
+					res.send(err);
+				
+				contact.name = req.body.name;
+				contact.email = req.body.email;
+
+				contact.save(function(err){
+					if (err)
+						res.send(err);
+					res.json({message: 'Contact updated'});
+				});
 			});
 		});
 
