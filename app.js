d6ecac3f4143a314ed215963c7459eef133f24c0
Chris Sharkey <ev2900@gmail.com>
2017-01-03 17:00:31 -0500

    working on an error

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ var router = express.Router();
 
 		//GET
 		//(single contact)
-		.get(function(req, res) {
+		.get(function(req, res) {	
 			Contact.findById(req.params.contact_id, function(err, contact) {
 				if(err)
 					res.send(err);
@@ -76,8 +76,7 @@ var router = express.Router();
 					res.send(err);
 				
 				contact.name = req.body.name;
-				contact.email = req.body.email;
-
+				
 				contact.save(function(err){
 					if (err)
 						res.send(err);
